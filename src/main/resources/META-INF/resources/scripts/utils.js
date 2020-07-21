550036a925f5595d378472ffaad98246f1c1f2af
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2020-07-21 16:46:06 -0400

    Qute+Vue.js UI

---
+++ src/main/resources/META-INF/resources/scripts/utils.js
@@ -0,0 +1,27 @@
+// Show snackbar
+function showSnackbar() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2000);
+}
+
+// get sessionId
+function getSessionId(reset){
+    if (sessionStorage.getItem('sessionId') === null || reset){
+        sessionStorage.setItem('sessionId', generateSessionId());
+    }
+    return sessionStorage.getItem('sessionId');
+}
+
+// generate sessionId
+function generateSessionId(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+
